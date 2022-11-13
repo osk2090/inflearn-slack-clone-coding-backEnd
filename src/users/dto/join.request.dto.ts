1ca60a0a61bb57c6b0db6d141ac6211f7d746110
osk2090 <osk2090@naver.com>
2022-11-13 15:21:44 +0900

    swagger 적용

--- src/users/dto/join.request.dto.ts
+++ src/users/dto/join.request.dto.ts
@@ -1,6 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class JoinRequestDto {
+  @ApiProperty({
+    example: 'osk2090@naver.com',
+    description: '이메일',
+    required: true,
+  })
   public email: string;
+
+  @ApiProperty({
+    example: 'osk2090',
+    description: '닉네임',
+    required: true,
+  })
   public nickname: string;
+
+  @ApiProperty({
+    example: '2090',
+    description: '비밀번호',
+    required: true,
+  })
   public password: string;
 
   constructor(email: string, nickname: string, password: string) {
