a8f295b8fcfb6d547b89effd9b8e3982f7d479f5
osk2090 <osk2090@naver.com>
2022-11-21 23:53:19 +0900

    transaction 적용

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,36 +5,66 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from '../entities/Users';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers';
+import { ChannelMembers } from '../entities/ChannelMembers';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users) private usersRepository: Repository<Users>,
+    // @InjectRepository(WorkspaceMembers)
+    // private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    // @InjectRepository(ChannelMembers)
+    // private channelMembersRepository: Repository<ChannelMembers>,
+    private dataSource: DataSource,
   ) {}
 
   getUser() {}
 
   async join(email: string, nickname: string, password: string) {
-    if (!email) {
-      throw new BadRequestException('이메일이 없습니다.');
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      if (!email) {
+        throw new BadRequestException('이메일이 없습니다.');
+      }
+      if (!nickname) {
+        throw new BadRequestException('닉네임이 없습니다.');
+      }
+      if (!password) {
+        throw new BadRequestException('비밀번호가 없습니다.');
+      }
+      const user = await queryRunner.manager
+        .getRepository(Users)
+        .findOne({ where: { email } });
+      if (user) {
+        throw new UnauthorizedException('이미 존재하는 사용자입니다.');
+      }
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const returned = await queryRunner.manager.getRepository(Users).save({
+        email,
+        nickname,
+        password: hashedPassword,
+      });
+      throw new Error('롤백!');
+      await queryRunner.manager.getRepository(WorkspaceMembers).save({
+        Userid: returned.id,
+        WorkspaceId: 1,
+      });
+      await queryRunner.manager.getRepository(ChannelMembers).save({
+        Userid: returned.id,
+        ChannelId: 1,
+      });
+      await queryRunner.commitTransaction();
+      return true;
+    } catch (error) {
+      console.log(error);
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
     }
-    if (!nickname) {
-      throw new BadRequestException('닉네임이 없습니다.');
-    }
-    if (!password) {
-      throw new BadRequestException('비밀번호가 없습니다.');
-    }
-    const user = await this.usersRepository.findOne({ where: { email } });
-    if (user) {
-      throw new UnauthorizedException('이미 존재하는 사용자입니다.');
-    }
-    const hashedPassword = await bcrypt.hash(password, 12);
-    await this.usersRepository.save({
-      email,
-      nickname,
-      password: hashedPassword,
-    });
   }
 }
