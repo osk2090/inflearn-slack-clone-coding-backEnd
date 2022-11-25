304e50d85945790cfa7c9dd856045a3a6b107e0c
osk2090 <osk2090@naver.com>
2022-11-25 23:38:27 +0900

    update

--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Workspaces } from '../entities/Workspaces';
+import { Repository } from 'typeorm';
+import { Channels } from '../entities/Channels';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers';
+import { ChannelMembers } from '../entities/ChannelMembers';
+import { Users } from '../entities/Users';
+import { ChannelChats } from '../entities/ChannelChats';
 
 @Injectable()
-export class ChannelsService {}
+export class ChannelsService {
+  constructor(
+    @InjectRepository(Workspaces)
+    private workspacesRepository: Repository<Workspaces>,
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+    @InjectRepository(ChannelChats)
+    private channelChatsRepository: Repository<ChannelChats>,
+  ) {}
+
+  async findById(id: number) {
+    return this.channelsRepository.find({ where: { id } });
+  }
+
+  async getWorkspaceChannels(url: string, myId: number) {
+    return this.channelsRepository
+      .createQueryBuilder('channels')
+      .innerJoinAndSelect(
+        'channels.ChannelMembers',
+        'channelMembers',
+        'channelMembers.userId = :myId',
+        { myId },
+      )
+      .innerJoinAndSelect(
+        'channels.Workspace',
+        'workspace',
+        'workspace.url = :url',
+        { url },
+      )
+      .getMany();
+  }
+
+  async getWorkspaceChannel(url: string, name: string) {
+    return this.channelsRepository.findOne({
+      where: { name },
+      relations: ['Workspace'],
+    });
+  }
+}
